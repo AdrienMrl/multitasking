41d61d40c4b2ac6f6b286d0b215bd9f878999fe6
Adrien Morel <adrien.morel@me.com>
2015-08-12 23:54:52 +0200

    invaders can now die

--- Assets/invader_controller.js
+++ Assets/invader_controller.js
@@ -1,11 +1,19 @@
 ﻿#pragma strict
 
-private var speed = 0.6f;
+private var speed = 1f;
+private var death_time = 0.3;
+private var dead : boolean = false;
+public var dead_sprite : Sprite;
 
 function Update () {
-    transform.position.y -= speed * Time.deltaTime;
+    if (!dead)
+        transform.position.y -= speed * Time.deltaTime;
 }
 
 function OnTriggerEnter2D(col : Collider2D) {
-    print("collide");
+    Destroy(col.gameObject);
+    GetComponent.<Animator>().enabled = false;
+    GetComponent.<SpriteRenderer>().sprite = dead_sprite;
+    Destroy(gameObject, death_time);
+    dead = true;
 }
